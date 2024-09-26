5277798ccc8b6bed01d8176a4293ff34ddb6e667
Ryan <rbuchanan.prsvr@gmail.com>
2024-09-25 21:57:06 -0400

    first commit

---
+++ ProductPage/src/components/Home.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const navigate = useNavigate();
+    function submitHandler(event){
+        event.preventDefault();
+        console.log("Date Submitted: ", {email, password});
+        if (email && password) {
+            navigate('/products');  // Navigate to the products page
+        }
+    }
+ return(
+    <div>
+        <h1>Shabby Chic</h1>
+        <h2>Login</h2>
+        <form onSubmit={submitHandler}>
+            <input
+                type="email"
+                onChange={(event)=>{setEmail(event.target.value);}}
+                value={email}
+                placeholder="Email" />
+            <input
+                type="password"
+                onChange={(event)=>{setPassword(event.target.value)}}
+                value={password}
+                placeholder="Password" />
+            <button>Submit</button>
+        </form>
+    </div>
+ );
+}
+export default Home;
---
+++ ProductPage/src/components/ProductDetails.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+function ProductDetails() {
+    return (
+        <div>
+            <h2>Product Details</h2>
+            {/* Add content here */}
+        </div>
+    );
+}
+export default ProductDetails;
+
+
+
